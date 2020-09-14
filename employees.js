7611a0fed91f3a3b5cfba720a65dee2716c8d4c9
Jason Tilley <jttilley007@yahoo.com>
2020-09-13 23:19:41 -0600

    fixed manager id check for add employee

--- employees.js
+++ employees.js
@@ -2,6 +2,13 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+// look up queries
+const deptQuery = `SELECT * FROM department`;
+const rolesQuery = `SELECT * FROM role`;
+const empNamesQuery = `SELECT id, CONCAT(employee.first_name, " ", employee.last_Name) AS name, role_id, manager_id FROM employee`;
+
+
+
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,6 +23,60 @@ var connection = mysql.createConnection({
   database: "employeesDB"
 });
 
+
+// functions to get lists for questions
+function getList(res, objElName)
+{
+  const arr = [];
+  console.log('res: ', res);
+  res.forEach(e => {
+    arr.push(e[objElName]);
+  });
+  return arr;
+}
+
+function listDepartments(deptRes) {
+  return getList(deptRes,"name");
+}
+
+function listRoles(roleRes) {
+  return getList(roleRes,"title");
+}
+
+function listEmployees(empRes) {
+  return getList(empRes,"name");
+}
+
+// functions to find ids for updates
+function findValue(res, srchEl, value, returnEl) {
+  console.log('returnEl: ', returnEl);
+  console.log('value: ', value);
+  console.log('srchEl: ', srchEl);
+
+  for(let i = 0; i < res.length; i++) {
+    console.log('res[i][srchEl]: ', res[i][srchEl]);
+    //for strings
+    if (isNaN(res[i][srchEl])) {
+      if (res[i][srchEl].match(value)) return res[i][returnEl];
+    } 
+    
+    // for numbers
+    if (res[i][srchEl] === value) {
+      return res[i][returnEl];
+    }
+  }
+}
+
+function findRoleId(roleRes,role) {
+  return findValue(roleRes,"title",role,"id")
+}
+
+function findEmpId(empRes, employee) {
+  return findValue(empRes,"name",employee,"id")
+}
+
+
+// SQL connection functions
 connection.connect(function(err) {
   if (err) throw err;
   displayTitle();
@@ -51,12 +112,13 @@ function start() {
       name: "action",
       type: "list",
       message: "What would you like to do?",
-      choices: ["Add Department",
-                "Add Roles",
-                "Add Employees",
+      choices: [
+                "View Employees",
                 "View Departments",
                 "View Roles",
-                "View Employees",
+                "Add Employees",
+                "Add Roles",
+                "Add Department",
                 "Update Employee Roles",
                 "Update Employee Managers",
                 "View Employees by Manager",
@@ -131,19 +193,16 @@ function addDept() {
   });
 }; 
 
+
 function addRoles() {
   // get departments
   let query = "SELECT * FROM department";
   connection.query(query, function(err, deptRes) {
     if (err) throw err;
 
-    const departments = [];
-    // console.log('res: ', res);
-    deptRes.forEach(e => {
-      departments.push(e.name);
-    });
-    departments.push("Cancel Go Back")
-    console.log('departments: ', departments);
+    const departments = listDepartments(deptRes)
+    departments.push("Cancel & Go Back")
+    // console.log('departments: ', departments);
 
     inquirer.prompt([
       {
@@ -170,9 +229,9 @@ function addRoles() {
         type: "list",
         message: "Which department is this role for?",
         choices: departments
-      },
+      }
     ]).then(({ role, salary, department }) => {
-      if (department === "Cancel Go Back") return start();
+      if (department === "Cancel & Go Back") return start();
 
       // get department id
       deptRes.forEach(e => {
@@ -189,24 +248,82 @@ function addRoles() {
   });
 }
 
-// var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-    //   query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-    //   query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+async function badaddEmp() {
+  // get roles
+  try {
+    const roleRes = await connection.query(rolesQuery);
+    
+    console.log('roleRes.title: ', roleRes.title);
+    
+    const roles = listRoles(roleRes);
+  
+    console.log('roles: ', roles);
+  
+      // get employee info
+    const { fName, lName, role } = await inquirer.prompt([
+      {
+        name: "fName",
+        type: "input",
+        message: "First Name:"
+      },
+      {
+        name: "lName",
+        type: "input",
+        message: "Last Name:"
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "role:",
+        choices: roles
+      }
+    ]);
+  
+    let res = await connection.query(empNamesQuery);
 
-    //query = "SELECT employee.id, role.title ";
-    // query += "FROM employee LEFT JOIN role ON (employee.role_id = role.id)"
+    // get role id
+    const roleId = findRoleId(roleRes,role);
+    
+    // get manager id
+    // checks role table for a role_id matching the roleId and returns that department id
+    const deptId = findValue(roleRes,"role_id",roleId,"department_id");
+    // checks the role table for a title that inlcudes "Lead" and the department id matches deptId then
+    // uses that roles id to check the employee table for the matching role_id and returns the employee id
+    for (let i = 0; i < roleRes.length; i++) {
+      if (roleRes[i].title.match("Lead") && roleRes[i].department_id === deptId) {
+        const managerId = findValue(res,"role_id",roleRes[i].id,"id");
+      }
+    }
+  
+    console.log('fName: ', fName);
+    console.log('lName: ', lName);
+    console.log('roleId: ', roleId);
+    console.log('managerId: ', managerId);
+    
+    res = await connection.query("INSERT INTO employee SET ?",
+      {
+        first_name: fName,
+        last_name: lName,
+        role_id: roleId, 
+        manager_id: managerId || null
+      }
+    );
+
+    console.log("Employee was successfully added!")
+    start();
+  } catch (err) {
+    throw err;
+  }
+}
+
 
 function addEmp() {
   // get roles
-  let query = "SELECT title, id FROM role";
-  connection.query(query, function(err, roleRes) {
+  connection.query(rolesQuery, function(err, roleRes) {
     if (err) throw err;
 
-    const roles = [];
-    // console.log('res: ', res);
-    roleRes.forEach(e => {
-      roles.push(e.title);
-    });
+    const roles = listRoles(roleRes);
+
     console.log('roles: ', roles);
 
     // get employee info
@@ -230,35 +347,31 @@ function addEmp() {
 
     ]).then( ({ fName, lName, role }) => {
 
-      
       query = "SELECT * FROM employee"
-      // console.log('query: ', query);
-
       connection.query(query, function(err, res) {
         if (err) throw err;
         
+        console.log('role: ', role);
+        console.log('roleRes: ', roleRes);
+        
         // get role id
-        roleRes.forEach(e => {
-          if (e.title === role) {
-            roleId = e.id;
-          }
-        });
-
-// console.log(res);
+        const roleId = findRoleId(roleRes,role);
+        console.log('roleId: ', roleId);
 
         // get manager id
+        // checks role table for a role_id matching the roleId and returns that department id
+        const deptId = findValue(roleRes,"id",roleId,"department_id");
+        console.log('deptId: ', deptId);
+        // checks the role table for a title that inlcudes "Lead" and the department id matches deptId then
+        // uses that roles id to check the employee table for the matching role_id and returns the employee id
         let managerId = null;
-        for (let i = 0; i < res.length; i++) {
-          console.log('res[i]: ', res[i]);
-          // assumes the manager is one with out a manager
-          // assume managers are the role id below the chosen role id
-          // probably need to make this a better check, maybe search for word matches with lead in there
-          if (res[i].manager_id === null && res[i].role_id === roleId-1) {
-            managerId = res[i].id
-            break;
+
+        for (let i = 0; i < roleRes.length; i++) {
+          if (roleRes[i].title.match("Lead") && roleRes[i].department_id === deptId) {
+            managerId = findValue(res,"role_id",roleRes[i].id,"id");
           }
         }
-
+  
         console.log('fName: ', fName);
         console.log('lName: ', lName);
         console.log('roleId: ', roleId);
@@ -277,9 +390,9 @@ function addEmp() {
             console.log("Employee was successfully added!")
             start();
           }
-        )
+        );
       });
-    });
+    })
   });
 }
 
@@ -327,7 +440,55 @@ function updateEmpManagers() {
 }
 
 function updateEmpRole() {
+  let query = `SELECT CONCAT(employee.first_name, " ", employee.last_Name) AS name, id FROM employee`;
+  connection.query(empNamesQuery,function(err, empRes) {
+    if (err) throw err;
 
+    const employees = listEmployees(empRes);
+    console.log('employees: ', employees);
+
+    connection.query(rolesQuery,function(err, roleRes) {
+      if (err) throw err;
+
+      const roles = listRoles(roleRes);
+      roles.push("Cancel & Go back");
+
+      inquirer.prompt([
+        {
+          name: "employee",
+          type: "list",
+          message: "Which employee would you like to change the role of?",
+          choices: employees
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "What is their new role?",
+          choices: roles
+        }
+      ]).then(({ employee, role }) => {
+        if (role === "Cancel & Go back") return start();
+
+        // find employee id for reference
+        let empId = findEmpId(empRes, employees);
+
+        // find role id
+        let roleId = findRoleId(roleRes, role);
+
+        let query = `UPDATE employee Set ? WHERE ?`
+        connection.query(query,[
+          { role_id: roleId },
+          { id: empId }
+        ],
+        function(err, res) {
+          if (err) throw err;
+          
+          console.log(`${employee} has the new role of ${role}!`);
+          start();
+        })
+      })
+    })
+  })
 }
 
 function delEmps() {
